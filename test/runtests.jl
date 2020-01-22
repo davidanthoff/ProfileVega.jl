46c4b234468cf0fb69ae53b968a82de02c003462
David Anthoff <anthoff@berkeley.edu>
2020-01-22 15:03:35 -0800

    Add a test

---
+++ test/runtests.jl
@@ -0,0 +1,24 @@
+using ProfileVega
+import VegaLite
+using Test
+
+function profile_test(n)
+    for i = 1:n
+        A = randn(100,100,20)
+        m = maximum(A)
+        Am = mapslices(sum, A; dims=2)
+        B = A[:,:,5]
+        Bsort = mapslices(sort, B; dims=1)
+        b = rand(100)
+        C = B.*b
+    end
+end
+
+
+@testset "ProfileVega" begin
+
+p1 = @profview profile_test(2)
+
+@test p1 isa VegaLite.VLSpec
+
+end
