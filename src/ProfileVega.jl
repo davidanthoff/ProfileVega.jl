6075c44fae84c0de631d6c644fe9cf198fad5971
David Anthoff <anthoff@berkeley.edu>
2020-01-24 09:51:11 -0800

    Add zoom

--- src/ProfileVega.jl
+++ src/ProfileVega.jl
@@ -8,7 +8,7 @@ export @profview, save
 function _add_node_to_data!(data, node, indent)
     push!(data, (level=indent, status=node.data.status==0 ? "Default" : node.data.status==1 ? "Runtime dispatch" : "Garbage collection", x1=node.data.span.start, x2=node.data.span.stop+.9, sf=string(node.data.sf)))
     for child in node
-        _add_node_to_data!(data, child, indent-1)
+        _add_node_to_data!(data, child, indent+1)
     end
 end
 
@@ -16,7 +16,20 @@ function _plotflamegraph(node)
     data = Vector{NamedTuple{(:level, :status, :x1, :x2, :sf), Tuple{Int,String,Float64,Float64,String}}}(undef, 0)    
     _add_node_to_data!(data, node, 0)
     
-    return data |> VegaLite.@vlplot(:bar, x={:x1, axis=nothing}, x2=:x2, y={"level:o", axis=nothing}, width=800, height=400, color={"status:n", legend={title=nothing, orient=:bottom}}, tooltip=:sf, title="Profile Results")
+    return data |> VegaLite.@vlplot(
+        :rect,
+        transform=[{calculate="datum.level+0.9", as=:level2}],
+        selection={grid={type=:interval, bind=:scales}},
+        x={:x1, axis=nothing},
+        x2=:x2,
+        y={"level:q", axis=nothing},
+        y2={"level2:q"},
+        width=800,
+        height=400,
+        color={"status:n", legend={title=nothing, orient=:bottom}},
+        tooltip=:sf,
+        title="Profile Results"
+    )
 end
 
 """
