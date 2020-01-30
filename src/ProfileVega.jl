08fdca7591b6cfa570cc274bc3bd4ba765c7e82b
Andrey Oskin <skoffer@arkoniak.com>
2020-01-30 07:05:55 +0200

    Second approach

--- src/ProfileVega.jl
+++ src/ProfileVega.jl
@@ -56,31 +56,112 @@ function _plotflamegraph(node; kwargs...)
     )
 end
 
+# No one expects the dynamic programming
+function lcs(node1::Vector{T}, node2::Vector{T}, eq = (x, y) -> x == y) where T
+    memo = Dict((T[], T[]) => Set{Vector{Tuple{T, T}}}())
+    stack = [(node1, node2)]
+    while !isempty(stack)
+        l1, l2 = stack[end]
+        if isempty(l1) || isempty(l2)
+            memo[(l1, l2)] = Set{Vector{Tuple{T, T}}}()
+            pop!(stack)
+            continue
+        end
+        if eq(l1[end], l2[end])
+            if !((l1[1:end - 1], l2[1:end - 1]) in keys(memo))
+                push!(stack, (l1[1:end - 1], l2[1:end - 1]))
+            else
+                pop!(stack)
+                if isempty(memo[(l1[1:end - 1], l2[1:end - 1])])
+                    union!(get!(memo, (l1, l2), Set{Vector{Tuple{T, T}}}()), Set([[(l1[end], l2[end])]]))
+                else
+                    for seq in memo[(l1[1:end - 1], l2[1:end - 1])]
+                        push!(seq, (l1[end], l2[end]))
+                    end
+                    union!(get!(memo, (l1, l2), Set{Vector{Tuple{T, T}}}()),
+                        (memo[(l1[1:end - 1], l2[1:end - 1])]))
+                end
+            end
+        else
+            if !((l1[1:end-1], l2) in keys(memo))
+                push!(stack, (l1[1:end-1], l2))
+            elseif !((l1, l2[1:end-1]) in keys(memo))
+                push!(stack, (l1, l2[1:end-1]))
+            else
+                pop!(stack)
+                p1 = isempty(memo[(l1[1:end-1], l2)]) ? 0 : length(collect(memo[(l1[1:end-1], l2)])[1])
+                p2 = isempty(memo[(l1, l2[1:end-1])]) ? 0 : length(collect(memo[(l1, l2[1:end-1])])[1])
 
-function flatten_tree(node, data = Dict{String, Int}())
-    data[string(node.data.sf)] = get!(data, string(node.data.sf), []), length(node.data.span))
-    for child in node
-        flatten_tree(child, data)
+                if p1 > p2
+                    union!(get!(memo, (l1, l2), Set{Vector{Tuple{T, T}}}()), memo[(l1[1:end-1], l2)])
+                elseif p1 < p2
+                    union!(get!(memo, (l1, l2), Set{Vector{Tuple{T, T}}}()), memo[(l1, l2[1:end-1])])
+                else
+                    union!(get!(memo, (l1, l2), Set{Vector{Tuple{T, T}}}()), memo[(l1[1:end-1], l2)])
+                    union!(get!(memo, (l1, l2), Set{Vector{Tuple{T, T}}}()), memo[(l1, l2[1:end-1])])
+                end
+            end
+        end
     end
-    data
+    memo[(node1, node2)]
 end
 
-function _add_diff_node_to_data(node, baseline, indent = 0, data = Vector{NamedTuple{(:level, :status, :x1, :x2, :sf), Tuple{Int,Int,Float64,Float64,String}}}(undef, 0))
-    diff = length(node.data.span) - get(baseline, string(node.data.sf), length(node.data.span))
+function _add_diff_node_to_data!(node, indent, status, data = Vector{NamedTuple{(:level, :status, :x1, :x2, :sf), Tuple{Int,Int,Float64,Float64,String}}}(undef, 0))
     push!(data,
         (level=indent,
-        status=diff,
+        status=status,
         x1=node.data.span.start,
         x2=node.data.span.stop+.9,
-        sf=string(node.data.sf)*"||"*string(length(node.data.span))*"||"*string(get(baseline, string(node.data.sf), 0))))
+        sf="$(clear_sf(node))\t$(length(node.data.span))\t0"))
     for child in node
-        _add_diff_node_to_data(child, baseline, indent+1, data)
+        _add_diff_node_to_data!(child, indent+1, 0, data)
     end
     data
 end
 
-function _plotdiffflamegraph(node, baseline; kwargs...)
-    data = _add_diff_node_to_data(node, baseline)
+function clear_sf(node)
+    # m = match(r"(.*)REPL\[[\d]+\](.*)", string(node.data.sf))
+    m = match(r".*REPL\[[\d]+\](.*)", string(node.data.sf))
+    m != nothing ? string(m[1]) : string(node.data.sf)
+end
+eq(x, y) = clear_sf(x) == clear_sf(y)
+
+function _add_diff_node_to_data(target::T, baseline::T, indent = 0, data = Vector{NamedTuple{(:level, :status, :x1, :x2, :sf), Tuple{Int,Int,Float64,Float64,String}}}(undef, 0)) where T
+    cb = collect(baseline)
+    if isempty(cb)
+        # no need to compare, may proceed further without baseline
+        _add_diff_node_to_data!(target, indent, length(target.data.span) - length(baseline.data.span), data)
+    else
+        ct = collect(target)
+        sequences = lcs(ct, cb, eq)
+        if isempty(sequences)
+            # target and baseline completely diverge, just proceed further without baseline
+            _add_diff_node_to_data!(target, indent, length(target.data.span) - length(baseline.data.span), data)
+        else
+            push!(data,
+                (level=indent,
+                status=length(target.data.span) - length(baseline.data.span),
+                x1=target.data.span.start,
+                x2=target.data.span.stop+.9,
+                sf="$(clear_sf(target))\t$(length(target.data.span))\t$(length(baseline.data.span))"))
+                #sf=clear_sf(target)))
+            #TODO first one will do for now, more complicated heuristics can be used later
+            seq = Dict(collect(sequences)[1])
+            for node in ct
+                if node in keys(seq)
+                    _add_diff_node_to_data(node, seq[node], indent+1, data)
+                else
+                    _add_diff_node_to_data!(node, indent+1, 0, data)
+                end
+            end
+        end
+    end
+    data
+end
+
+
+function _plotdiffflamegraph(target, baseline; kwargs...)
+    data = _add_diff_node_to_data(target, baseline)
 
     return data |> VegaLite.@vlplot(
         mark={type=:rect, stroke="#505050"},
@@ -129,10 +210,10 @@ View profiling results in comparison to baseline profile. See [FlameGraphs](http
 for options for `kwargs`.
 """
 function viewdiff(baseline, data::Vector{UInt64}=Profile.fetch(); kwargs...)
-    g = FlameGraphs.flamegraph(data; filter(x -> !(x[1] in fieldnames(Options)), kwargs)...)
-    baseline = flatten_tree(FlameGraphs.flamegraph(baseline; filter(x -> !(x[1] in fieldnames(Options)), kwargs)...))
+    target = FlameGraphs.flamegraph(data; filter(x -> !(x[1] in fieldnames(Options)), kwargs)...)
+    baseline = FlameGraphs.flamegraph(baseline; filter(x -> !(x[1] in fieldnames(Options)), kwargs)...)
 
-    return _plotdiffflamegraph(g, baseline; kwargs...)
+    return _plotdiffflamegraph(target, baseline; kwargs...)
 end
 
 """
